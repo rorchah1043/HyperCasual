9b92db6c05a62d94cec6146c8ab69e17974fd55a
Vsevolod Merenkov <loredan725@gmail.com>
2023-07-15 23:56:28 +0300

    [feat] Добавлено ускорение спавна и периодические волны

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,24 +2,43 @@
 
 public class EnemySpawner : MonoBehaviour
 {
-    public float spawnPeriod;
     public GameObject enemyPrefab;
     public GameObject wall;
 
-    private float _lastSpawnTime;
+    public float spawnFrequency;
+    public float spawnFrequencyAcceleration;
+
+    public float waveLength;
+    public float waveMultiplier;
+    public float wavePeriod;
+
+    private float _lastSpawnTime = float.NegativeInfinity;
+    private float _lastWaveStartTime;
 
     private void Update()
     {
-        if (Time.time > _lastSpawnTime + spawnPeriod)
+        if (Time.time - _lastWaveStartTime > wavePeriod)
+        {
+            _lastWaveStartTime = Time.time;
+        }
+
+        if (Time.time > _lastSpawnTime + 1 / (spawnFrequency * (IsWave() ? waveMultiplier : 1)))
         {
             Spawn();
             _lastSpawnTime = Time.time;
         }
+
+        spawnFrequency += spawnFrequencyAcceleration * Time.deltaTime;
     }
 
     private void Spawn()
     {
         var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
         enemy.GetComponent<Enemy>().wall = wall;
+    }
+
+    public bool IsWave()
+    {
+        return Time.time - _lastWaveStartTime < waveLength;
     }
 }
---
+++ Assets/Scripts/WaveWarning.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WaveWarning : MonoBehaviour
+{
+    [SerializeField] private EnemySpawner enemySpawner;
+    [SerializeField] private GameObject warningObject;
+
+    [SerializeField] private float blinkOnDuration;
+    [SerializeField] private float blinkOffDuration;
+
+    private float _lastBlinkTime = float.NegativeInfinity;
+
+    private void Update()
+    {
+        if (enemySpawner.IsWave())
+        {
+            var lastBlinkTimeAgo = Time.time - _lastBlinkTime;
+            if (lastBlinkTimeAgo > blinkOnDuration + blinkOffDuration)
+            {
+                _lastBlinkTime = Time.time;
+                warningObject.SetActive(true);
+            }
+            else if (lastBlinkTimeAgo > blinkOnDuration)
+            {
+                warningObject.SetActive(false);
+            }
+            else
+            {
+                warningObject.SetActive(true);
+            }
+        }
+        else
+        {
+            warningObject.SetActive(false);
+        }
+    }
+}
